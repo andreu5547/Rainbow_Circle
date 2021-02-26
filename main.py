f4cfcedc969131e094b88466200b16987693f8d6
andreu5547 <andreu5547@mail.ru>
2021-02-26 21:32:43 +0300

    first commit

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 import sys
-
-from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter, QColor
 import random
+from PyQt5 import QtCore, QtWidgets
 
 
-class MyWidget(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.start = QtWidgets.QPushButton(self.centralwidget)
+        self.start.setGeometry(QtCore.QRect(700, 560, 93, 28))
+        self.start.setObjectName("start")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Git и желтые окружности"))
+        self.start.setText(_translate("MainWindow", "Start"))
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.start.clicked.connect(self.paint)
         self.do_paint = False
 
@@ -30,8 +49,13 @@ class MyWidget(QMainWindow):
         qp.drawEllipse(random.randint(0, self.width()), random.randint(0, self.height()), r, r)
 
 
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyWidget()
     ex.show()
+    sys.excepthook = except_hook
     sys.exit(app.exec_())
